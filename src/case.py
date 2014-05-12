842d28936967f1da515d0e85c109bea36acb05f2
mesnardo <mesnardo@gwu.edu>
2014-05-12 16:57:12 -0400

    Create file case.py that contains class Case.
    The class Case contains the name of the case (e.g. cylinder)
    and its path.

---
+++ src/case.py
@@ -0,0 +1,14 @@
+# source: $pyIBM/src/case.py
+# Olivier Mesnard (mesnardo@gwu.edu)
+# BarbaGroup (lorenabarba.com)
+		
+import os
+
+
+class Case:
+	'''Class to define the name and the path
+	of the case.
+	'''
+	def __init__(self,loc):
+		Case.path = os.getcwd()+'/'+loc
+		Case.name = os.path.basename(os.path.normpath(Case.path))
