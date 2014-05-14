b542098ab7a687971862577517502e7917b69c06
mesnardo <mesnardo@gwu.edu>
2014-05-14 17:01:00 -0400

    Modification of the docstrings.

--- src/operations.py
+++ src/operations.py
@@ -3,14 +3,12 @@
 # BarbaGroup (lorenabarba.com)
 
 def grad(u, direction):
-	'''Compute the gradient of 'u' in a a given direction.'''
-
+	'''Computes the gradient of a variable in a a given direction.'''
 	if direction == 'x':
 		return u.gradientx.mat.dot(u.field) + u.gradientx.bc_vect
 	elif direction == 'y':
 		return u.gradienty.mat.dot(u.field) + u.gradienty.bc_vect
 
 def lap(u):
-	'''Compute the laplacian of a vector.'''
-
+	'''Computes the laplacian of a variable.'''
 	return u.laplacian.mat.dot(u.field) + u.laplacian.bc_vect
