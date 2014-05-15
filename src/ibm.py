1293f99f35b7ce65c6e015a1c545ee54e16b73fa
mesnardo <mesnardo@gwu.edu>
2014-05-15 17:30:31 -0400

    Use enumerate and zip instead of xrange to loop over the surface.

--- src/ibm.py
+++ src/ibm.py
@@ -10,79 +10,79 @@ from solver import Solver
 
 
 def dh(r):
-	'''Compute the delta function,
+	'''Computes the delta function,
 	using the formulation from Roma et al. (1999).
 	'''
 	r = abs(r)
-	if 0 <= r < 1:
+	if 0. <= r < 1.:
 		return 0.125*(3-2*r+sqrt(1+4*r-4*r**2))
-	elif 1 <= r < 2:
+	elif 1. <= r < 2.:
 		return 0.125*(5-2*r-sqrt(-7+12*r-4*r**2))
 	else:
 		return 0.0
 
 def interpolation(u, body):
-	'''Interpolate the velocity at the Lagrangian points,
-	using values of the Eulerian of the neighbors.
+	'''Interpolates the velocity at the Lagrangian points,
+	using values of the surrounding Eulerian points.
 	'''
 	uk = np.zeros(body.N, dtype=float)
-	for k in xrange(body.N):
-		h = ( Mesh.x[body.neighbor[k]%Mesh.Nx+1]
-			- Mesh.x[body.neighbor[k]%Mesh.Nx] )
+	for k, (neighbor, x, y) in enumerate(zip(body.neighbor, body.x, body.y)):
+		h = ( Mesh.x[neighbor%Mesh.Nx+1]
+			- Mesh.x[neighbor%Mesh.Nx] )
 		for i in xrange(-2,3):
 			for j in xrange(-2,3):
-				I = body.neighbor[k]%Mesh.Nx+i
-				J = body.neighbor[k]/Mesh.Nx+j
+				I = neighbor%Mesh.Nx + i
+				J = neighbor/Mesh.Nx + j
 				uk[k] += ( u[J*Mesh.Nx+I]
-						 * dh((body.x[k]-Mesh.x[I])/h)
-						 * dh((body.y[k]-Mesh.y[J])/h) )
+					     * dh((x - Mesh.x[I])/h)
+					     * dh((y - Mesh.y[J])/h) )
 	return uk
 
 def distribution(fk, body):
-	'''Spread the Lagrangian force
+	'''Distributes the Lagrangian forces
 	onto the Eulerian grid.
 	'''
 	f = np.zeros(Mesh.Nx*Mesh.Ny, dtype=float)
-	for k in xrange(body.N):
-		h = (Mesh.x[body.neighbor[k]%Mesh.Nx+1]
-			- Mesh.x[body.neighbor[k]%Mesh.Nx])
+
+	for neighbor, x, y, f_k in zip(body.neighbor, body.x, body.y, fk):
+		h = ( Mesh.x[neighbor%Mesh.Nx+1]
+			- Mesh.x[neighbor%Mesh.Nx] )
 		for i in xrange(-2,3):
 			for j in xrange(-2,3):
-				I = body.neighbor[k]%Mesh.Nx+i
-				J = body.neighbor[k]/Mesh.Nx+j
-				f[J*Mesh.Nx+I] += ( fk[k]
-								  * dh((body.x[k]-Mesh.x[I])/h)
-								  * dh((body.y[k]-Mesh.y[J])/h) )
+				I = neighbor%Mesh.Nx + i
+				J = neighbor/Mesh.Nx + j
+				f[J*Mesh.Nx+I] += ( f_k
+								  * dh((x - Mesh.x[I])/h)
+								  * dh((y - Mesh.y[J])/h) )
 	return f
 
 
 def ibm(body, u, v):
 	'''Immersed boundary method.'''
-
 	fx = np.zeros(Mesh.Nx*Mesh.Ny, dtype=float)
 	fy = np.zeros(Mesh.Nx*Mesh.Ny, dtype=float)
 
 	if body.is_moving:
 		body.kinematics()
 	
+	N_ibm = 1
 	body.cl,body.cd = 0.,0.
-
-	for i in xrange(1):
+	for ite in xrange(N_ibm):
 		body.u = interpolation(u.field, body)
 		body.v = interpolation(v.field, body)
-		body.fx[:] = (body.ud[:]-body.u[:])/Solver.dt
-		body.fy[:] = (body.vd[:]-body.v[:])/Solver.dt
+		body.fx[:] = (body.ud[:] - body.u[:]) / Solver.dt
+		body.fy[:] = (body.vd[:] - body.v[:]) / Solver.dt
 
-		for k in xrange(body.N):
-			body.cd += ( -2.*body.fx[k]
-					   * Mesh.dx[body.neighbor[k]%Mesh.Nx]
-					   * Mesh.dy[body.neighbor[k]/Mesh.Nx] )
-			body.cl += ( -2.*body.fy[k]
-					   * Mesh.dx[body.neighbor[k]%Mesh.Nx]
-					   * Mesh.dy[body.neighbor[k]/Mesh.Nx] )
+		for neighbor, fx_b, fy_b in zip(body.neighbor, body.fx, body.fy):
+			body.cd += ( -2.*fx_b
+					   * Mesh.dx[neighbor%Mesh.Nx]
+					   * Mesh.dy[neighbor/Mesh.Nx] )
+			body.cl += ( -2.*fy_b
+					   * Mesh.dx[neighbor%Mesh.Nx]
+					   * Mesh.dy[neighbor/Mesh.Nx] )
 
 		fx += distribution(body.fx, body)
 		fy += distribution(body.fy, body)
 
-	u.field[:] += Solver.dt*fx[:]
-	v.field[:] += Solver.dt*fy[:]
+	u.field[:] += Solver.dt * fx[:]
+	v.field[:] += Solver.dt * fy[:]
