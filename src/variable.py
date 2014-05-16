536ac649ca0da4b62d42c0ae0d6ab9436eb7259c
mesnardo <mesnardo@gwu.edu>
2014-05-16 10:46:42 -0400

    Reads and Writes files using numpy functions: loadtxt() and savetxt().

--- src/variable.py
+++ src/variable.py
@@ -13,9 +13,10 @@ from mesh import Mesh
 from solver import Solver
 from matrix import *
 
+
 class BoundaryConditions:
-	'''Create the boundary condition with the type and value.
-	Type: Dirichlet or Neummann.
+	'''Creates the boundary condition with the type and value.
+	Type of boundary condition: Dirichlet or Neummann.
 	'''
 	def __init__(self, info_bc):
 		self.left = [info_bc['left'][0],info_bc['left'][1] * np.ones(Mesh.Ny, dtype=float)]
@@ -23,26 +24,22 @@ class BoundaryConditions:
 		self.bottom = [info_bc['bottom'][0],info_bc['bottom'][1] * np.ones(Mesh.Nx, dtype=float)]
 		self.top = [info_bc['top'][0],info_bc['top'][1] * np.ones(Mesh.Nx, dtype=float)]
 
-class Variable:
-	'''Create a variable.'''
 
+class Variable:
+	'''Creates a variable.'''
 	def __init__(self, name, skip_assemble=False):
 		self.name = name
 		infile = open(Case.path+'/_infoFlow.yaml', 'r')
 		info = yaml.load(infile)
-		infile.close()
-		
-		# initial conditions
+		infile.close()	
+		# get initial conditions or read a data file
 		if Solver.start == 0:
 			self.field = ( info[self.name]['initialCondition']
 						 * np.ones(Mesh.Nx*Mesh.Ny, dtype=float) )
 		else:
-			self.field = np.empty(Mesh.Nx*Mesh.Ny, dtype=float)
 			self.read()
-
-		# boundary condition
+		# boundary conditions
 		self.bc = BoundaryConditions(info[self.name]['boundaryCondition'])
-	
 		# assemble matrices
 		if not skip_assemble:
 			infile = open(Case.path+'/_infoScheme.yaml', 'r')
@@ -55,32 +52,19 @@ class Variable:
 									 scheme=d['scheme'], 
 									 direction=d['direction'])
 
-
 	def assemble_matrix(self, name, scheme, direction):
-		'''Assemble a matrix related to a variable.'''
-
+		'''Assembles a matrix related to a variable,
+		calling the class Matrix.
+		'''
 		setattr(self, name+direction, 
 				Matrix(self.bc, name, scheme, direction))
 
-
 	def read(self):
-		'''Read the variable field from a file.'''
-
-		infile = open(Case.path+'/'+str(Solver.ite)+'/'+self.name+'.dat', 'r')
-		i = 0
-		for line in infile:
-			data = line.split()
-			self.field[i] = float(data[0])
-			i += 1
-		infile.close()
-
+		'''Reads the variable field from a file.'''
+		self.field = np.loadtxt(fname=Case.path+'/'+str(Solver.ite)+'/'+self.name+'.dat', dtype=float)
 
 	def write(self):
-		'''Write the variable field into a file.'''
-
+		'''Writes the variable field into a file.'''
 		if not os.path.isdir(Case.path+'/'+str(Solver.ite)):
 			os.system('mkdir '+Case.path+'/'+str(Solver.ite))
-		outfile = open(Case.path+'/'+str(Solver.ite)+'/'+self.name+'.dat', 'w')
-		for i in xrange(len(self.field)):
-			outfile.write(str(self.field[i])+'\n')
-		outfile.close()
+		np.savetxt(fname=Case.path+'/'+str(Solver.ite)+'/'+self.name+'.dat', X=self.field, fmt='%.6f')
