09d4699fe327a5d73b1a719e0ae0cdcbdf635b9d
mesnardo <mesnardo@gwu.edu>
2014-09-03 16:16:12 -0400

    Adapt code with new definition of bound conditions

--- src/matrix.py
+++ src/matrix.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+
 import numpy as np
 from scipy.sparse import *
 import yaml
@@ -24,6 +25,7 @@ class Matrix:
 		direction -- gradient's direction.
 		"""
 		self.bc = bc
+		# call the right function with respect to the name
 		self.mat, self.bc_vect = getattr(self, 'set_'+name)(direction, scheme)
 	
 	def set_gradient(self, direction, scheme='central'):
@@ -53,11 +55,11 @@ class Matrix:
 					I, J = i/Nx, i%Nx
 				#bc1
 				if I == 0:
-					if bc1[0] == 'dirichlet':
+					if bc1.bc_type == 'dirichlet':
 						A.append([i, i, 1./d[I]])
-						B[i] += -bc1[1][J]/d[I]
-					elif bc1[0] == 'neumann':
-						B[i] += bc1[1][J]
+						B[i] += -bc1.values[J]/d[I]
+					elif bc1.bc_type == 'neumann':
+						B[i] += bc1.values[J]
 				else:
 					A.append([i, i-p, -1./d[I-1]])
 					A.append([i, i, 1./d[I-1]])
@@ -69,23 +71,24 @@ class Matrix:
 					I, J = i/Nx, i%Nx
 				#bc1
 				if I == 0:
-					if bc1[0] == 'dirichlet':
+					if bc1.bc_type == 'dirichlet':
 						A.append([i, i+p, 0.5/d[I]])
-						B[i] += -0.5*bc1[1][J]/d[I]
-					elif bc1[0] == 'neumann':
-						B[i] += bc1[1][J]
+						B[i] += -0.5*bc1.values[J]/d[I]
+					elif bc1.bc_type == 'neumann':
+						B[i] += bc1.values[J]
 				#bc2
 				elif I == N-1:
-					if bc2[0] == 'dirichlet':
+					if bc2.bc_type == 'dirichlet':
 						A.append([i, i-p, -0.5/d[I-1]])
 						A.append([i, i, 0.5*(1./d[I-1]-1./d[I])])
-						B[i] += 0.5*bc2[1][J]/d[I]
-					elif bc2[0] == 'neumann':
-						B[i] += bc2[1][J]
+						B[i] += 0.5*bc2.values[J]/d[I]
+					elif bc2.bc_type == 'neumann':
+						B[i] += bc2.values[J]
 				else:
 					A.append([i, i-p, -0.5/d[I-1]])
 					A.append([i, i, 0.5*(1./d[I-1]-1./d[I])])
 					A.append([i, i+p, 0.5/d[I]])
+		
 		val, row, col = [x[2] for x in A], [x[0] for x in A], [x[1] for x in A]
 		
 		return csr_matrix((val, (row, col)), shape=(Nx*Ny, Nx*Ny), dtype=float), B
@@ -109,38 +112,38 @@ class Matrix:
 			if I == 0:
 				A.append([i, i+1, 1./dx[I]**2])
 				A.append([i, i, -2./dx[I]**2])
-				if bc.left[0] == 'dirichlet':
-					B[i] += bc.left[1][J]/dx[I]**2
-				elif bc.left[0] == 'neumann':
+				if bc.left.bc_type == 'dirichlet':
+					B[i] += bc.left.values[J]/dx[I]**2
+				elif bc.left.bc_type == 'neumann':
 					A.append([i, i, 1./dx[I]**2])
-					B[i] += -bc.left[1][J]/dx[I]
+					B[i] += -bc.left.values[J]/dx[I]
 			#right
 			if I == Nx-1:
 				A.append([i, i-1, 1./dx[I]**2])
 				A.append([i, i, -2./dx[I]**2])
-				if bc.right[0] == 'dirichlet':
-					B[i] += bc.right[1][J]/dx[I]**2
-				elif bc.right[0] == 'neumann':
+				if bc.right.bc_type == 'dirichlet':
+					B[i] += bc.right.values[J]/dx[I]**2
+				elif bc.right.bc_type == 'neumann':
 					A.append([i, i, 1./dx[I]**2])
-					B[i] += bc.right[1][J]/dx[I]
+					B[i] += bc.right.values[J]/dx[I]
 			#bottom
 			if i < Nx:
 				A.append([i, i+Nx, 1./dy[J]**2])
 				A.append([i, i, -2./dy[J]**2])
-				if bc.bottom[0] == 'dirichlet':
-					B[i] += bc.bottom[1][I]/dy[J]**2
-				elif bc.bottom[0] == 'neumann':
+				if bc.bottom.bc_type == 'dirichlet':
+					B[i] += bc.bottom.values[I]/dy[J]**2
+				elif bc.bottom.bc_type == 'neumann':
 					A.append([i, i, 1./dy[J]**2])
-					B[i] += -bc.bottom[1][I]/dy[J]
+					B[i] += -bc.bottom.values[I]/dy[J]
 			#top
 			if i >= Nx*(Ny-1):
 				A.append([i, i-Nx, 1./dy[J]**2])
 				A.append([i, i, -2./dy[J]**2])
-				if bc.top[0] == 'dirichlet':
-					B[i] += bc.top[1][I]/dy[J]**2
-				elif bc.top[0] == 'neumann':
+				if bc.top.bc_type == 'dirichlet':
+					B[i] += bc.top.values[I]/dy[J]**2
+				elif bc.top.bc_type == 'neumann':
 					A.append([i, i, 1./dy[J]**2])
-					B[i] += bc.top[1][I]/dy[J]
+					B[i] += bc.top.values[I]/dy[J]
 			#point not on left and not on right
 			if I != 0 and I != Nx-1:
 				A.append([i, i-1, 2./dx[I-1]/(dx[I-1]+dx[I])])
@@ -151,5 +154,7 @@ class Matrix:
 				A.append([i, i-Nx, 2./dy[J-1]/(dy[J-1]+dy[J])])
 				A.append([i, i, -2./dy[J-1]/dy[J]])
 				A.append([i, i+Nx, 2./dy[J]/(dy[J-1]+dy[J])])
+		
 		val, row, col = [x[2] for x in A], [x[0] for x in A], [x[1] for x in A]
+		
 		return csr_matrix((val, (row, col)), shape=(Nx*Ny, Nx*Ny), dtype=float), B
