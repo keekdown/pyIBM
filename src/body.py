6829f76d70e0833189108e95aece2f273e0c0b21
cmit <keekdown17@ya.ru>
2015-03-24 13:48:53 +0600

    Some fixes

--- src/body.py
+++ src/body.py
@@ -1,92 +1,92 @@
 # file: $pyIBM/src/body.py
 # author: Olivier Mesnard (mesnardo@gwu.edu)
 # BarbaGroup (lorenabarba.com)
-		
+        
 
 from math import sin, cos, pi
 import numpy as np
 import yaml
-
+from parameters import Parameters
 from case import Case
 from mesh import Mesh
 from solver import Solver
 
 
 class Body:
-	"""Creates an immersed boundary."""
-	def __init__(self):
-		"""Parses the file _infoBody.yaml 
-		and generates the immersed boundary.
-		"""
-		# parse the file
-		with open(Case.path+'/_infoBody.yaml', 'r') as infile:
-			info = yaml.load(infile)['IB']
-		# store info
-		self.name = info['name']
-		self.coord_file = info['coordinates']
-		self.is_moving = info['moving']
-		# generate the body
-		self.generate()
+    """Creates an immersed boundary."""
+    def __init__(self):
+        """Parses the file _infoBody.yaml 
+        and generates the immersed boundary.
+        """
+        # parse the file
+        with open(Case.path+'/_infoBody.yaml', 'r') as infile:
+            info = yaml.load(infile)['IB']
+        # store info
+        self.name = info['name']
+        self.coord_file = info['coordinates']
+        self.is_moving = info['moving']
+        # generate the body
+        self.generate()
 
-	def generate(self):
-		"""Generates the immersed boundary, finds neighbors
-		and initializes Lagrangian variables.
-		"""
-		# read the coordinate file
-		with open(Case.path+'/'+self.coord_file, 'r') as infile:
-			self.x, self.y = np.loadtxt(infile, dtype=float, 
-										delimiter='\t', unpack=True)
-		#TODO:bug with generate points by only x axis
-		self.N = len(self.x)    # number of body points
-		print ('\n-> Number of points on the body: ', self.N, '\n')
-		
-		# compute the length vector
-		self.get_length()
-	
-		# find the neighbors
-		self.get_neighbors()
-		
-		# initialize other Lagrangian variables
-		self.u = np.empty(self.N, dtype=float)
-		self.v = np.empty(self.N, dtype=float)
-		self.fx = np.empty(self.N, dtype=float)
-		self.fy = np.empty(self.N, dtype=float)
-		self.ud = np.zeros(self.N, dtype=float)
-		self.vd = np.zeros(self.N, dtype=float)
-		self.x0, self.y0 = np.copy(self.x), np.copy(self.y)
+    def generate(self):
+        """Generates the immersed boundary, finds neighbors
+        and initializes Lagrangian variables.
+        """
+        # read the coordinate file
+        with open(Case.path+'/'+self.coord_file, 'r') as infile:
+            self.x, self.y = np.loadtxt(infile, dtype=float, 
+                                        delimiter='\t', unpack=True)
+        #TODO:bug with generate points by only x axis
+        self.N = len(self.x)    # number of body points
+        print ('\n-> Number of points on the body: ', self.N, '\n')
+        
+        # compute the length vector
+        self.get_length()
+    
+        # find the neighbors
+        self.get_neighbors()
+        
+        # initialize other Lagrangian variables
+        self.u = np.empty(self.N, dtype=float)
+        self.v = np.empty(self.N, dtype=float)
+        self.fx = np.empty(self.N, dtype=float)
+        self.fy = np.empty(self.N, dtype=float)
+        self.ud = np.zeros(self.N, dtype=float)
+        self.vd = np.zeros(self.N, dtype=float)
+        self.x0, self.y0 = np.copy(self.x), np.copy(self.y)
 
-	def get_length(self):
-		"""Computes the distance between two Lagrangian points
-		in each direction for each body point.
-		"""
-		self.dx = np.empty(self.N, dtype=float)
-		self.dy = np.empty(self.N, dtype=float)
-		self.dx[1:self.N] = self.x[1:self.N] - self.x[0:self.N-1]
-		self.dy[1:self.N] = self.y[1:self.N] - self.y[0:self.N-1]
-		self.dx[0] = self.x[0] - self.x[-1]
-		self.dy[0] = self.y[0] - self.y[-1]
+    def get_length(self):
+        """Computes the distance between two Lagrangian points
+        in each direction for each body point.
+        """
+        self.dx = np.empty(self.N, dtype=float)
+        self.dy = np.empty(self.N, dtype=float)
+        self.dx[1:self.N] = self.x[1:self.N] - self.x[0:self.N-1]
+        self.dy[1:self.N] = self.y[1:self.N] - self.y[0:self.N-1]
+        self.dx[0] = self.x[0] - self.x[-1]
+        self.dy[0] = self.y[0] - self.y[-1]
 
-	def get_neighbors(self):
-		"""Finds the closest Eulerian point on the mesh grid."""
-		self.neighbor = np.empty(self.N, dtype=int)
-		for k, (x, y) in enumerate(zip(self.x, self.y)):
-			for i in range(Mesh.Nx-1):
-				if Mesh.x[i] <= x < Mesh.x[i+1]:
-					I = i
-					break
-			for j in range(Mesh.Ny-1):
-				if Mesh.y[j] <= y < Mesh.y[j+1]:
-					J = j
-					break
-			self.neighbor[k] = J*Mesh.Nx + I
+    def get_neighbors(self):
+        """Finds the closest Eulerian point on the mesh grid."""
+        self.neighbor = np.empty(self.N, dtype=int)
+        for k, (x, y) in enumerate(zip(self.x, self.y)):
+            for i in range(Mesh.Nx-1):
+                if Mesh.x[i] <= x < Mesh.x[i+1]:
+                    I = i
+                    break
+            for j in range(Mesh.Ny-1):
+                if Mesh.y[j] <= y < Mesh.y[j+1]:
+                    J = j
+                    break
+            self.neighbor[k] = J*Mesh.Nx + I
 
-	def kinematics(self):
-		"""Defines the kinematics of a moving immersed boundary."""
-		A = 1.0
-		Nf = 100
-		f = 1./(Nf*Solver.dt)
-		self.x[:] = self.x0[:] + 0.0
-		self.x[:] = self.y0[:] + A*sin(2*pi*f*Solver.ite*Solver.dt)
-		self.ud[:] = 0.0
-		self.vd[:] = 2*pi*f*A*cos(2*pi*f*Solver.ite*Solver.dt)
-		self.getNeighbors()
+    def kinematics(self):
+        """Defines the kinematics of a moving immersed boundary."""
+        A = 1.0
+        Nf = 100
+        f = 1./(Nf*Parameters.dt)
+        self.x[:] = self.x0[:] + 0.0
+        self.x[:] = self.y0[:] + A*sin(2*pi*f*Parameters.ite*Parameters.dt)
+        self.ud[:] = 0.0
+        self.vd[:] = 2*pi*f*A*cos(2*pi*f*Parameters.ite*Parameters.dt)
+        self.get_neighbors()
--- src/solver.py
+++ src/solver.py
@@ -19,129 +19,132 @@ from input_output import timer_start, timer_stop
 
 
 class Solver:
-	"""Creates a solver."""
-	def __init__(self, skip_assemble=False, skip_poisson=False):
-		"""Initializes the solver.
-		
-		Creates the flow variables, 
-		assembles the discretized operator matrices
-		and initializes the Poisson solver.
-		"""
-		# read the simulation parameters
-		Parameters()
+    """Creates a solver."""
+    def __init__(self, skip_assemble=False, skip_poisson=False):
+        """Initializes the solver.
 
-		# initialize flow variables
-		self.initialize_variables()
+        Creates the flow variables,
+        assembles the discretized operator matrices
+        and initializes the Poisson solver.
+        """
+        # read the simulation parameters
+        Parameters()
 
-		print ('\n{Assembling matrices}')
-		tic = timer_start()
-		if not skip_assemble:
-			self.assemble_matrices()
-		else:
-			print ('--> skipped')
-		timer_stop(tic, info='Assembling matrices')
+        # initialize flow variables
+        self.initialize_variables()
+        self.initialize_solver_variables()
 
-		if not skip_poisson:
-			Solver.poisson = Poisson(Solver.p)
-		else:
-			print ('--> Poisson skipped')
+        print ('\n{Assembling matrices}')
+        tic = timer_start()
+        if not skip_assemble:
+            self.assemble_matrices()
+        else:
+            print ('--> skipped')
+        timer_stop(tic, info='Assembling matrices')
 
-	def initialize_variables(self):
-		with open(Case.path+'/_infoFlow.yaml', 'r') as infile:
-			info_variables = yaml.load(infile)
-		for name, info in info_variables.items():
-			setattr(Solver, name, Variable(name, info))
-	
-	#def initialize_solver_variables(self):
-	#	with open(Case.path+'/_infoSolver.yaml','r') as infile:
-	#		info_solver = yaml.load(infile)
-	
-	def assemble_matrices(self):
-		Solver.u.assemble_matrices()
-		Solver.v.assemble_matrices()
-		Solver.p.assemble_matrices()
+        if not skip_poisson:
+            Solver.poisson = Poisson(Solver.p)
+        else:
+            print ('--> Poisson skipped')
 
-	def solve(self, body=None):
+    def initialize_variables(self):
+        with open(Case.path+'/_infoFlow.yaml', 'r') as infile:
+            info_variables = yaml.load(infile)
+        for name, info in info_variables.items():
+            setattr(Solver, name, Variable(name, info))
 
-		# open the file that contains force coefficients
-		if body:
-			outfile = open(Case.path+'/forceCoeffs.dat',
-					   	  ('w' if Parameters.start == 0 else 'a'))
+    def initialize_solver_variables(self):
+        with open(Case.path+'/_infoSolver.yaml','r') as infile:
+            info_solver = yaml.load(infile)
+            for name, info in info_solver.items():
+                setattr(Solver, name, info)
 
-		# time-integration using Euler method
-		tic = timer_start()
-		while Parameters.ite < Parameters.start + Parameters.nt:
-			Parameters.ite += 1
-			print ('\nIteration %d - Time = %f \\'
-				  % (Parameters.ite, Parameters.ite*Parameters.dt) )
+    def assemble_matrices(self):
+        Solver.u.assemble_matrices()
+        Solver.v.assemble_matrices()
+        Solver.p.assemble_matrices()
 
-			# get intermediate velocity
-			self.intermediate_velocity()
+    def solve(self, body=None):
 
-			# immersed boundary method
-			self.immersed_boundary_method(body)
+        # open the file that contains force coefficients
+        if body:
+            outfile = open(Case.path+'/forceCoeffs.dat',
+                             ('w' if Parameters.start == 0 else 'a'))
 
-			# solve Poisson equation for pressure
-			self.solve_poisson()
+        # time-integration using Euler method
+        tic = timer_start()
+        while Parameters.ite < Parameters.start + Parameters.nt:
+            Parameters.ite += 1
+            print ('\nIteration %d - Time = %f \\'
+                  % (Parameters.ite, Parameters.ite*Parameters.dt) )
 
-			# update velocity field
-			self.update_velocity()
+            # get intermediate velocity
+            self.intermediate_velocity()
 
-			# write variable fields
-			if Parameters.ite%Parameters.write_every == 0:
-				print ('\n{Writting results}')
-				if not os.path.isdir(Case.path+'/'+str(Parameters.ite)):
-					os.system('mkdir '+Case.path+'/'+str(Parameters.ite))
-				Solver.u.write()
-				Solver.v.write()
-				Solver.p.write()
-			# write force coefficients
-			if body:
-				outfile.write('%f \t %f \t %f' 
-							  % (Parameters.ite*Parameters.dt, body.cl, body.cd))
-		
-		timer_stop(tic, info='DONE')
-		# close the file containing force coefficients
-		if body:
-			outfile.close()
+            # immersed boundary method
+            self.immersed_boundary_method(body)
 
-	def intermediate_velocity(self):
-		"""Computes the intermediate velocity field.
-		
-		Solves the Navier-Stokes equations,
-		without the pressure gradient and boundardy forces,
-		using Euler's method.
-		"""
-		Solver.u.field += Parameters.dt * (
-							1./Parameters.Re*lap(Solver.u)
-							- Solver.u.field*grad(Solver.u, 'x')
-							- Solver.v.field*grad(Solver.u, 'y'))
-		Solver.v.field += Parameters.dt * (
-							1./Parameters.Re*lap(Solver.v)
-							- Solver.u.field*grad(Solver.v, 'x')
-							- Solver.v.field*grad(Solver.v, 'y'))
+            # solve Poisson equation for pressure
+            self.solve_poisson()
 
-	def immersed_boundary_method(self, body=None):
-		"""Udpates the velocity field 
-		with the effect of the immersed boundary.
-		
-		Arguments
-		---------
-		body -- immersed boundary (default None).
-		"""
-		if body:
-			Solver.u.field, Solver.v.field = ibm(body, Solver.u.field, Solver.v.field)
-	
-	def solve_poisson(self):
-		"""Solves the Poisson equation for the pressure field."""
-		b = 1./Parameters.dt * (grad(Solver.u, 'x') + grad(Solver.v, 'y'))
-		Solver.p.field = Solver.poisson.solve(Solver.p.laplacian.mat, b-Solver.p.laplacian.bc_vect, Solver.p.field)
-		print ('{Poisson} Number of iterations: %d \\'
-			  % Solver.poisson.ite)
+            # update velocity field
+            self.update_velocity()
 
-	def update_velocity(self):
-		"""Updates the velocity field with pressure correction
-		to make the field divergence-free.
-		"""
-		Solver.u.field -= Parameters.dt * grad(Solver.p, 'x')
-		Solver.v.field -= Parameters.dt * grad(Solver.p, 'y')
+            # write variable fields
+            if Parameters.ite%Parameters.write_every == 0:
+                print ('\n{Writting results}')
+                if not os.path.isdir(Case.path+'/'+str(Parameters.ite)):
+                    os.system('mkdir '+Case.path+'/'+str(Parameters.ite))
+                Solver.u.write()
+                Solver.v.write()
+                Solver.p.write()
+            # write force coefficients
+            if body:
+                outfile.write('%f \t %f \t %f'
+                              % (Parameters.ite*Parameters.dt, body.cl, body.cd))
+
+        timer_stop(tic, info='DONE')
+        # close the file containing force coefficients
+        if body:
+            outfile.close()
+
+    def intermediate_velocity(self):
+        """Computes the intermediate velocity field.
+
+        Solves the Navier-Stokes equations,
+        without the pressure gradient and boundardy forces,
+        using Euler's method.
+        """
+        Solver.u.field += Parameters.dt * (
+                            1./Parameters.Re*lap(Solver.u)
+                            - Solver.u.field*grad(Solver.u, 'x')
+                            - Solver.v.field*grad(Solver.u, 'y'))
+        Solver.v.field += Parameters.dt * (
+                            1./Parameters.Re*lap(Solver.v)
+                            - Solver.u.field*grad(Solver.v, 'x')
+                            - Solver.v.field*grad(Solver.v, 'y'))
+
+    def immersed_boundary_method(self, body=None):
+        """Udpates the velocity field
+        with the effect of the immersed boundary.
+
+        Arguments
+        ---------
+        body -- immersed boundary (default None).
+        """
+        if body:
+            Solver.u.field, Solver.v.field = ibm(body, Solver.u.field, Solver.v.field)
+
+    def solve_poisson(self):
+        """Solves the Poisson equation for the pressure field."""
+        b = 1./Parameters.dt * (grad(Solver.u, 'x') + grad(Solver.v, 'y'))
+        Solver.p.field = Solver.poisson.solve(Solver.p.laplacian.mat, b-Solver.p.laplacian.bc_vect, Solver.p.field)
+        print ('{Poisson} Number of iterations: %d \\'
+              % Solver.poisson.ite)
+
+    def update_velocity(self):
+        """Updates the velocity field with pressure correction
+        to make the field divergence-free.
+        """
+        Solver.u.field -= Parameters.dt * grad(Solver.p, 'x')
+        Solver.v.field -= Parameters.dt * grad(Solver.p, 'y')
